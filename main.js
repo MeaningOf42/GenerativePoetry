4aa3faa61ee71f4b7fb335b547d251ad8ff697bc
Billy Tmini <billy_timimi@protonmail.ch>
2020-05-25 00:36:43 +0100

    updated poem again

--- main.js
+++ main.js
@@ -8,10 +8,10 @@ embers of a once love that had been inferno long ago
 turn too ash; lose hope`;
   var poem_fire = `
 A fire is a living being you care for
-Just like a lover flames can warm you, protect you, or hurt you more than anything
+Just like a lover,  flames can warm you, protect you, or hurt you more than anything
 A beauty too as she dances
 Tired now shes almost gone
-But the wind may blow life into the flames once more, and the embers may turn once more to a roaring furnace!`;
+But the wind may blow life into the flames once more, and the embers may again become a roaring furnace!`;
 
   for (var i = 0; i<poem_girl.split("\n").length; i++) {
     poem_txt += [poem_girl, poem_fire][Math.floor(Math.random()*2)].split("\n")[i];
